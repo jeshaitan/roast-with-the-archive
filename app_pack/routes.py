73d7879b0944c832cf0191945a92df138e413670
Jesse Smith <jesse.etan.smith@gmail.com>
2021-01-29 15:39:26 -0500

    google cloud instance request/hosting

--- app_pack/routes.py
+++ app_pack/routes.py
@@ -3,12 +3,16 @@ from app_pack import app
 from app_pack.lineform import LineForm
 from app_pack.gen import startsess, genresp
 from random import randint
-import tensorflow as tf
+#import tensorflow as tf
 
+import requests
+
+'''
 # Start a Tensorflow/GPT2 session and load from checkpoint
-CHECKPOINT = 'run3_med_titled_fast'
+CHECKPOINT = 'eot_small'
 tfsess = startsess(CHECKPOINT)
 graph = tf.get_default_graph()
+'''
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -23,22 +27,28 @@ def index():
     if lform.sbutton.data and lform.validate_on_submit():
         if session['nothingsofar']:
             session['nothingsofar'] = False
-            session['acc_roast'] = '<|title|>' + lform.lines.data + '\n'
+            session['acc_roast'] = lform.lines.data + '\n'
         else:
-            session['acc_roast'] = '<|title|>' + session['acc_roast'] + '\n' + lform.lines.data + '\n'
+            session['acc_roast'] = session['acc_roast'] + '\n' + lform.lines.data + '\n'
 
         # Generate response from entire roast so far.
         prompt = session['acc_roast']
-        resplength = max(1, len(lform.lines.data) + randint(0, 5)) 
+        resplength = min(len(lform.lines.data.split()) * 3, 1023)
+        
+        '''
         with graph.as_default():
-            response = genresp(tfsess,
-                               temp=0.70,
+            session['acc_roast'] = genresp(tfsess,
                                pref=prompt,
-                               top_p=0.95,
                                length=resplength,
                                checkpoint=CHECKPOINT)
-            # Remove title token
-            session['acc_roast'] = response[9:]
+        '''
+        req = requests.post('https://gpt2-api-jeivmljjkq-nn.a.run.app',
+                    json={'length': resplength,
+                          'temperature': 0.7,
+                          'top_p': 0.95,
+                          'prefix': prompt,
+                          'truncate': '<|endoftext|>'})
+        session['acc_roast'] = req.json()['text']
 
     lform.sbutton.disabled = False
     lform.cbutton.disabled = False
