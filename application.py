f12e47eb4218d432a8ca6c6bb38846d6eae4243c
Jesse Smith <jesse.etan.smith@gmail.com>
2021-01-29 18:50:53 -0500

    herokuport

--- application.py
+++ application.py
@@ -4,4 +4,5 @@ application = app
 
 if __name__ == "__main__":
     #application.debug = True
-    application.run()
+    port = int(os.environ.get("PORT", 5000))
+    application.run(host='0.0.0.0', port=port)
